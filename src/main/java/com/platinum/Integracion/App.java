1c2b87999c74638cac22629b75a8d2a70f936da2
RobinsonConcha <121482451+RobinsonConcha@users.noreply.github.com>
2022-12-28 03:12:18 -0400

    Proyecto para prueba desde jenkins

---
+++ src/main/java/com/platinum/Integracion/App.java
@@ -0,0 +1,25 @@
+package com.platinum.Integracion;
+
+/**
+ * Hello world!
+ * 
+ * @author Robinson 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    
+    {  
+    	System.out.println("--------------");
+        System.out.println(" Hello World!");
+        System.out.println("-------------");
+        System.out.println("😎🎉🤑🤑🤑🤑🎉😎");
+        System.out.println("--------------");
+        System.out.println("--Bienvenido--");
+        System.out.println("--------------");
+        System.out.println("Banco Platinum");
+        System.out.println("--------------");
+       
+    }
+}
